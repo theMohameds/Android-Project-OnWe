c6e7a1741bd7577413c87c1735e268ec48932c8e
CSteentoft <143799991+CSteentoft@users.noreply.github.com>
2025-10-30 12:10:30 +0100

    mvvm setup with example files.
    (cherry picked from commit 213913d516b8507806a2aa456d53a6fa3f243e52)

---
+++ app/src/main/java/com/example/android_project_onwe/model/model.kt
@@ -0,0 +1,10 @@
+package com.example.android_project_onwe.model
+
+
+/*
+MODEL:
+( Reusable Code – DATA ) Business Objects that encapsulate data and behavior of application domain,
+Simply hold the data.
+ */
+class model {
+}
---
+++ app/src/main/java/com/example/android_project_onwe/viewmodel/viewmodel.kt
@@ -0,0 +1,8 @@
+package com.example.android_project_onwe.viewmodel
+
+/*
+VIEWMODEL: ( Reusable Code – LOGIC ) Link between Model and View OR It Retrieves data from Model and exposes it to the View.
+This is the model specifically designed for the View.
+ */
+class viewmodel {
+}
